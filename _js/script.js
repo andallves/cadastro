9017097cb0c831d0990be7c2ea4f74455100e9ef
Andre Alves <37510133+andallves@users.noreply.github.com>
2023-03-08 22:35:53 -0300

    Atualizações no layout

--- _js/script.js
+++ _js/script.js
@@ -1,1 +1,6 @@
-console.log('Hello world!')
+const senha = document.getElementById(#password)
+const confirme_password = document.getElementById(#confirme-password)
+
+if (senha !== confirm_password) {
+  var message = "Senhas não coincidem";
+}
